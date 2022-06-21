f3fb61e307523153bb6f0d58e209e442917f00d4
RamajAlaa5 <ramojaalaa15@gmail.com>
2022-06-22 00:10:54 +0200

    Project Structure and Migration

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,21 @@
+<template>
+
+<div>
+<HomeApp/>
+<router-view></router-view>
+</div>
+</template>
+
+<script>
+import HomeApp from"./home.vue"
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+        components:{
+            HomeApp
+        }
+    }
+</script>
+<style scoped>
+</style>
---
+++ resources/js/components/home.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#" style="color:black">Home</a>
+        <a class="nav-link" href="#" style="color:black">TodoList</a>
+
+      </li>
+      </ul>
+      </div>
+      </nav>
+</div>
+
+
+</template>
+
+<script>
+    export default {
+       name:"HomeApp"
+    }
+</script>
